76882a69eac8f1476815dedaac1b26b1ea764269
mahmoud-elgarnosy <mh1644@fayoum.edu.eg>
2021-07-06 15:26:16 +0200

    collect_life_hand to make the model more robust

---
+++ EDA/collecting_real_data.py
@@ -0,0 +1,39 @@
+from HandDetector import handDetector
+import cv2
+import os
+import time
+
+detector = handDetector()
+path  = r'testing/real_data'
+
+for i in range(0,10):
+    cap = cv2.VideoCapture(0)
+    path2 = path+f'/{i}/'
+    os.makedirs(path2, exist_ok=True)
+    saved = 0 
+    while True:
+        new_path = path2 + f'{saved}.png'
+        sucess, img =cap.read()
+        img, x_min, x_max, y_min, y_max = detector.cropHand(img, draw=False)
+        cv2.putText(img,str(i) , (10, 70),cv2.FONT_HERSHEY_COMPLEX_SMALL, 
+                    3,(255, 0, 255), 5)
+        cv2.imshow("Image", img)
+        if x_min and  x_max and y_min and y_max:
+            topLeft = (x_min - 25, y_min - 25)
+            bottomRight = (x_max + 25,y_max + 25)
+            x, y = topLeft[0], topLeft[1]
+            w, h = bottomRight[0] - topLeft[0], bottomRight[1] - topLeft[1]
+            ROI = img[y:y+h, x:x+w]
+            if 0 not in ROI.shape:
+                ROI = cv2.resize(ROI, (224,224))
+                cv2.imwrite(new_path, ROI)
+                saved +=1
+                print(saved)
+        
+        if cv2.waitKey(1) & 0xFF == ord('q') or saved >=500 :
+            break
+    
+    time.sleep(2)
+    cv2.destroyAllWindows()
+    cap.release()
+    print(saved)
