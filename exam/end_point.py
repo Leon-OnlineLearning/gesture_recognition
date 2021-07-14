5f52eca7ab179c3011cedec7e35354bbdd21d0fd
mahmoud-elgarnosy <mh1644@fayoum.edu.eg>
2021-07-14 13:29:32 +0200

    receive video function

---
+++ exam/end_point.py
@@ -0,0 +1,29 @@
+import os
+from flask import Flask,request,Response,jsonify
+from flask_api import status
+from werkzeug.utils import secure_filename
+from flask import send_from_directory
+import numpy as np
+from gesture_recognition import gesture_recognition
+
+UPLOAD_FOLDER = './recordings'
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+@app.route('/exams/<user_id>', methods=[ 'POST'])
+def receving_chunks_during_exam(user_id):
+    file = request.files['chunk'] #the video
+    if file and file.filename != '':
+        filename = secure_filename(file.filename)
+        datapath=os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(datapath)
+        gesture = gesture_recognition(datapath)
+        os.remove(datapath)
+        return jsonify({'gesture':gesture})
+
+    #if there is no file or bad file recived return 400
+    return "Inavalid video type", status.HTTP_400_BAD_REQUEST
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=50000, debug=True)
