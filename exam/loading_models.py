d4eb03a5c018976281246ea8194169a044c23a4c
mahmoud-elgarnosy <mh1644@fayoum.edu.eg>
2021-07-14 13:27:49 +0200

    create singleton_model class

---
+++ exam/loading_models.py
@@ -0,0 +1,19 @@
+from tensorflow import keras
+from EDA.HandDetector import handDetector
+
+class Singleton_model:
+   __model = None
+   __detector = None
+   @staticmethod 
+   def getInstance():
+      if Singleton_model.__model == None:
+         Singleton_model()
+      return Singleton_model.__model,Singleton_model.__detector
+
+   
+   def __init__(self, min_detection_confidence= .9, min_tracking_confidence=.9):
+      if Singleton_model.__model != None:
+         raise Exception("This class is a singleton!")
+      else:
+         Singleton_model.__model = keras.models.load_model('../Gesture recognition_Sign Language_mobilenet_')
+         Singleton_model.__detector= handDetector(min_detection_confidence, min_tracking_confidence)
